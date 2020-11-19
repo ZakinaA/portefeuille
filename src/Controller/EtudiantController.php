cce9415fc0ccc2ab89a07ff6ed9a1a8668034a01
laubris <leo.aubrispro@gmail.com>
2020-11-19 11:11:21 +0100

    Correction route

--- src/Controller/EtudiantController.php
+++ src/Controller/EtudiantController.php
@@ -1,26 +1,25 @@
 <?php
-
+ 
 namespace App\Controller;
-
+ 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\HttpFoundation\Response;
+ 
 class EtudiantController extends AbstractController
 {
-    /**
-     * @Route("/etudiant", name="etudiant2")
+    /*
+     * @Route("/etudiant", name="etudiant")
      */
     public function index()
     {
-     	/* Cette simple instruction permet d'envoyer des informations au navigateur sans passer par une vue.
+        /* Cette simple instruction permet d'envoyer des informations au navigateur sans passer par une vue.
         return new Response('<html><body>Salut Les SIO</body></html>');
         */
  
          // initialise une variable qui sera exploitée dans la vue
          $annee = '2020';
-         return $this->render('etudiant/accueil.html.twig', ['pAnnee' => $annee,
-        ]);				         
+         return $this->render('etudiant/vue_accueil.html.twig', ['pAnnee' => $annee,
+        ]);                      
     }
-
 }
