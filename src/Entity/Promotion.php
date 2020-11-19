499339ae9b46a3c4c833fe945f43f34610d2324a
claraN <claranoel144@gmail.com>
2020-11-19 14:35:25 +0100

    Ajout entités et relations

--- src/Entity/Promotion.php
+++ src/Entity/Promotion.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\PromotionRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -26,6 +28,16 @@ class Promotion
      * @ORM\Column(type="integer", nullable=true)
      */
     private $annee;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Etudiant::class, mappedBy="promotion")
+     */
+    private $etudiants;
+
+    public function __construct()
+    {
+        $this->etudiants = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -52,6 +64,36 @@ class Promotion
     public function setAnnee(?int $annee): self
     {
         $this->annee = $annee;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Etudiant[]
+     */
+    public function getEtudiants(): Collection
+    {
+        return $this->etudiants;
+    }
+
+    public function addEtudiant(Etudiant $etudiant): self
+    {
+        if (!$this->etudiants->contains($etudiant)) {
+            $this->etudiants[] = $etudiant;
+            $etudiant->setPromotion($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEtudiant(Etudiant $etudiant): self
+    {
+        if ($this->etudiants->removeElement($etudiant)) {
+            // set the owning side to null (unless already changed)
+            if ($etudiant->getPromotion() === $this) {
+                $etudiant->setPromotion(null);
+            }
+        }
 
         return $this;
     }
