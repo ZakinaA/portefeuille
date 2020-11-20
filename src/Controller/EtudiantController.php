4de19cfafca6dcacb745cdc0a94e1eadeab5a210
dubosqN <noe.dubosq.allende@gmail.com>
2020-11-20 06:55:19 +0100

    "Trie_Table + bug fix"

--- src/Controller/EtudiantController.php
+++ src/Controller/EtudiantController.php
@@ -16,10 +16,7 @@ class EtudiantController extends AbstractController
         /* Cette simple instruction permet d'envoyer des informations au navigateur sans passer par une vue.
         return new Response('<html><body>Salut Les SIO</body></html>');
         */
- 
-         // initialise une variable qui sera exploitée dans la vue
-         $annee = '2020';
-         return $this->render('etudiant/vue_accueil.html.twig', ['pAnnee' => $annee,
-        ]);                      
+
+         return $this->render('etudiant/trie_table.html.twig');
     }
 }
