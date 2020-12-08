e0823c3bfd959da75f5d893fa182d2c95a886762
dubosqN <noe.dubosq.allende@gmail.com>
2020-12-08 11:39:33 +0100

    test unitaires

--- src/Controller/ActiviteController.php
+++ src/Controller/ActiviteController.php
@@ -77,15 +77,11 @@ class ActiviteController extends AbstractController
  
      //SELECT jour.libelle, domaine_taches.libelle, tache_semaine.duree, tache_semaine.description from domaine_taches, tache_semaine, semaine_stage, stage, jour where stage.id = semaine_stage.stage_id and semaine_stage.id = tache_semaine.semaine_stage_id and tache_semaine.domaine_id = domaine_taches.id and tache_semaine.jour_id = jour.id and stage.id = 16;
 
-    public function consulterSemaine($stage_id){
-        $stage = $this->getDoctrine()->getRepository(Stage::class)->find($stage_id);
-        //var_dump($stage);
-        if(!$stage){
-            echo "Aucun";
-        }
+    public function consulterSemaine($semaineStage_id){
+        $semaineStage = $this->getDoctrine()->getRepository(SemaineStage::class)->find($semaineStage_id);
 
-
+            var_dump($semaineStage);
         return $this->render('activite/consulterSemaine.html.twig', [
-            'paramStage' => $stage,]);          
+            'pSemaineStage' => $semaineStage,]);          
     }   
 }
