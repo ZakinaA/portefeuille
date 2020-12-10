170b49cec90c1b13596a506b3e89710a85026575
Tomchv1 <chauvintom.pro@gmail.com>
2020-12-10 09:00:00 +0100

    Uniformisation visuelle form ajout RP

--- src/Form/RPType.php
+++ src/Form/RPType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
@@ -18,9 +19,9 @@ class RPType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('libcourt', TextType::class, array('label' => 'Libellé court'))
-            ->add('descriptif', TextType::class)
-            ->add('contexte', TextType::class)
+            ->add('libcourt', TextType::class)
+            ->add('descriptif', TextareaType::class)
+            ->add('contexte', TextareaType::class)
             ->add('dateDebut', DateType::class, array('input' => 'datetime',
                                                           'widget' => 'single_text',  
                                                           'required' => true,
@@ -33,16 +34,8 @@ class RPType extends AbstractType
                                                           'placeholder' => 'jj/mm/aaaa'))
             ->add('environnement', TextType::class)
             ->add('moyen', TextType::class)
-            ->add('dateModif', DateType::class, array('input' => 'datetime',
-                                                          'widget' => 'single_text',  
-                                                          'required' => true,
-                                                          'label' =>'Date de mosdification',
-                                                          'placeholder' => 'jj/mm/aaaa'))
-            ->add('localisation', EntityType::class, array('class' => 'App\Entity\Localisation','choice_label' => 'Libelle' ))
-            ->add('statut' , EntityType::class, array('class' => 'App\Entity\Statut','choice_label' => 'Libelle' ))
+            ->add('localisation', EntityType::class, array('class' => 'App\Entity\Localisation','choice_label' => 'Libelle' ))  
             ->add('source', EntityType::class, array('class' => 'App\Entity\Source','choice_label' => 'Libelle' ))
-            ->add('etudiant', EntityType::class, array('class' => 'App\Entity\Etudiant','choice_label' => 'Nom' ))
-            ->add('enseignant', EntityType::class, array('class' => 'App\Entity\Enseignant','choice_label' => 'Nom' ))
             ->add('cadre', EntityType::class, array('class' => 'App\Entity\Cadre','choice_label' => 'Libelle' ))
         ;
     }
