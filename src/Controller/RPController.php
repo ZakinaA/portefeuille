18c3e3738b4771255ccd7263c0da5b260385f32a
HugoClemt <hugoclemt@gmail.com>
2020-12-15 08:13:34 +0100

    Stabilisation

--- src/Controller/RPController.php
+++ src/Controller/RPController.php
@@ -46,7 +46,7 @@ class RPController extends AbstractController
 
         $repository = $this->getDoctrine()->getRepository(RP::class);
         $RPaModifier = $repository->findBy(
-            ['etudiant' => $etudiant_id, 'statut' => 3]);
+            ['etudiant' => $etudiant_id, 'statut' => 3], array('libcourt'=>'asc'));
         
         return $this->render('rp/lister.html.twig', ['pRP' => $RPaModifier]);
     }
